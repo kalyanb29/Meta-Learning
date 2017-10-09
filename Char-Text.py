7b81f70af5936e932eadfe7eccbe197cba7b8852
kalyanb29 <kalyan.iitkgp08@gmail.com>
2017-10-10 09:19:53 +1100

    Add files via upload
    in Tensorflow Character level Text generation

--- Char-Text.py
+++ Char-Text.py
@@ -45,59 +45,64 @@ for ii in range(num_sentences):
     Y[ii] = text_1hot[cp + 1: cp + max_sequence_length + 1]
     cp += mb_increment
 
-x = tf.placeholder(tf.float32,[None, max_sequence_length, n_chars])
-y_ = tf.placeholder(tf.float32,[None, max_sequence_length, n_chars])
-cell = tf.nn.rnn_cell.LSTMCell(hidden_size,state_is_tuple=True)
-val,state = tf.nn.dynamic_rnn(cell,x,dtype = tf.float32)
-val = tf.transpose(val,[1,0,2])
-last = tf.gather(val,val.get_shape().as_list()[0] - 1)
-weight = tf.Variable(tf.truncated_normal([hidden_size,y_.get_shape().as_list()[2]]))
-bias = tf.Variable(tf.constant(0.1,shape = [y_.get_shape().as_list()[2]]))
-prediction = tf.nn.softmax(tf.matmul(last, weight) + bias)
-cross_entropy = -tf.reduce_sum(y_ * tf.log(tf.clip_by_value(prediction,1e-10,1.0)))
-optimizer = tf.train.AdamOptimizer()
-minimize = optimizer.minimize(cross_entropy)
-mistakes = tf.not_equal(tf.argmax(y_, 1), tf.argmax(prediction, 1))
-error = tf.reduce_mean(tf.cast(mistakes, tf.float32))
+x = tf.placeholder(tf.float32,[None,max_sequence_length,n_chars])
+y_ = tf.placeholder(tf.float32,[None,max_sequence_length,n_chars])
+y_out = []
+cell = tf.contrib.rnn.MultiRNNCell(
+                     [tf.contrib.rnn.BasicLSTMCell(hidden_size) for _ in range(2)])
+cell_out, state = tf.nn.dynamic_rnn(cell,x, dtype = tf.float32)
+loss = 0.0
+for i in range(max_sequence_length):
+    if i > 0:tf.get_variable_scope().reuse_variables()
+    cell_out_slice = cell_out[:,i]
+    softmax_w = tf.get_variable("softmax_w", [hidden_size, n_chars])
+    softmax_b = tf.get_variable("softmax_b", [n_chars])
+    output_slice = tf.add(tf.matmul(cell_out_slice, softmax_w), softmax_b)
+    y_slice = y_[:,i]
+    y_out.append(output_slice)
+    loss += tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = output_slice,labels=y_slice))
+loss = loss/max_sequence_length
+train_op = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+output_op = tf.stack(y_out,axis = 1)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for epoch in range(100):
+        chosen_idx = np.random.choice(X.shape[0], replace=False, size=20)
+        x_s = X[chosen_idx,]
+        y_s = Y[chosen_idx]
+        cost, _ = sess.run([loss, train_op], feed_dict={x: x_s, y_: y_s})
+        print("Epoch %d : loss %f" % (epoch, cost))
+    saver = tf.train.Saver()
+    saver.save(sess, "model", global_step=0)
 
-init_op = tf.initialize_all_variables()
-sess = tf.Session()
-sess.run(init_op)
-batch_size = 20
-no_of_batches = int(len(X)/batch_size)
-epoch = 10
-for i in range(epoch):
-    ptr = 0
-    for j in range(no_of_batches):
-        inp, out = X[ptr:ptr+batch_size], Y[ptr:ptr+batch_size]
-        ptr+=batch_size
-        sess.run(minimize,{x: inp, y_: out})
-    print("Epoch - ",str(i))
-# incorrect = sess.run(error,{x: test_input, y_: test_output})
-# print('Epoch {:2d} error {:3.1f}%'.format(i + 1, 100 * incorrect))
-# sess.close()
+tf.reset_default_graph()
+saver = tf.train.Saver()
+with tf.Session() as sess:
+  # Restore variables from disk.
+    saver.restore(sess, "model")
+    next_char = np.random.randint(n_chars)
+    for kk in range(10):
+        cseed = np.zeros((1, max_sequence_length, n_chars), np.int32)
+        for ii in range(0, max_sequence_length):
+            cseed[0,ii,next_char] = 1
+            out_emb = sess.run(output_op,feed_dict ={x:cseed})
+            next_char = np.random.choice(n_chars, p=out_emb[0,ii])
+    print("".join([token_lookup.get(x,"") for x in cseed[0].argmax(axis=1)]))
 
-#     y = tf.matmul(output,softmax_w) + softmax_b
 # model = Sequential()
-# model.add(layers.LSTM(hidden_size,
-#                            input_shape=(max_sequence_length, n_chars),
-#                            return_sequences=True))
-# model.add(layers.LSTM(hidden_size,
-#                            input_shape=(max_sequence_length, n_chars),
-#                            return_sequences=True))
+# model.add(layers.LSTM(hidden_size,input_shape=(max_sequence_length, n_chars),return_sequences=True))
+# model.add(layers.LSTM(hidden_size,input_shape=(max_sequence_length, n_chars),return_sequences=True))
 # model.add(layers.TimeDistributed(layers.Dense(n_chars, activation='softmax')))
 # model.compile('adam', 'categorical_crossentropy', metrics=['accuracy'])
-# model.summary()
-# model.fit(X, Y, batch_size=20, epochs=10)
-# model.save("char_rnn_model_l2_h512.h5")
+# # # tvar = model.trainable_weights
+# history = model.fit(X, Y, batch_size=20, epochs=100)
+# # model.save("char_rnn_model_l2_h512.h5")
 # model = models.load_model("char_rnn_model_l2_h512.h5")
-# sequence_length = 60
 # next_char = np.random.randint(n_chars)
 # for kk in range(10):
-#     cseed = np.zeros((1, max_sequence_length, n_chars), np.int32)
-#     for ii in range(0, sequence_length):
-#         cseed[0,ii,next_char] = 1
-#         out_emb = model.predict(cseed)
-#         next_char = np.random.choice(n_chars, p=out_emb[0,ii])
-#
-#     print("".join([token_lookup.get(x,"") for x in cseed[0].argmax(axis=1)]))
+#    cseed = np.zeros((1, max_sequence_length, n_chars), np.int32)
+#    for ii in range(0, sequence_length):
+#        cseed[0,ii,next_char] = 1
+#        out_emb = model.predict(cseed)
+#        next_char = np.random.choice(n_chars, p=out_emb[0,ii])
+# print("".join([token_lookup.get(x,"") for x in cseed[0].argmax(axis=1)]))

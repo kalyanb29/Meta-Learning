5f02ab4be7f7e9de108071903abad4e3733c238c
Kalyan <kalyan.iitkgp08@gmail.com>
2017-11-05 20:35:16 +1100

    Updated

--- MetaLearn.py
+++ MetaLearn.py
@@ -8,10 +8,7 @@ from keras.layers import Input, Dense
 from tensorflow.python.util import nest
 import os
 import collections, mock
-import sonnet as snt
 from timeit import default_timer as timer
-from tensorflow.contrib.learn.python.learn import monitored_session as ms
-logging  = tf.logging
 from keras.models import Sequential, Model
 from keras.utils import np_utils
 # f = np.load('mnist.npz')
@@ -27,21 +24,19 @@ from keras.utils import np_utils
 # y_test = np_utils.to_categorical(y_test)
 # Testing
 
-seed = 7
-np.random.seed(seed)
-n_epoch = 10
-num_steps = 100
+n_epoch = 30
+num_steps = 50
 evaluation_period = 10
 evaluation_epochs = 20
 log_period = 10
-batch_size = 100
-num_dims = 10
+batch_size = 128
+num_dims = 2
 num_layer = 2
-hidden_size = 20
-unroll_nn = 20
+hidden_size = 5
+unroll_nn = 5
 lr = 0.001
-logs_path = '/Users/kalyanb/PycharmProjects/MetaLearning/MetaLog/'
-save_path = "/Users/kalyanb/PycharmProjects/MetaLearning/MetaOpt/model.ckpt"
+logs_path = '/Users/Admin/Desktop/TensorFlow/MetaLog/'
+save_path = '/Users/Admin/Desktop/TensorFlow/MetaOpt/model.ckpt'
 
 
 def _wrap_variable_creation(func, custom_getter):
@@ -135,7 +130,7 @@ def metaopti(prob):
         with tf.name_scope("gradients"):
             shapes = [K.get_variable_shape(p) for p in x]
             grads = K.gradients(fx, x)
-            grads, _ = tf.clip_by_global_norm(grads, 5.0)
+            # grads, _ = tf.clip_by_global_norm(grads, 5.0)
             grads = [tf.stop_gradient(g) for g in grads]
         with tf.variable_scope('MetaNetwork'):
             cell_count = 0
@@ -180,22 +175,21 @@ def metaopti(prob):
         return delta, S_C_out,S_H_out
 
     def time_step(t,f_array,x,state_c,state_h):
-        x_new = x
         with tf.name_scope('Unroll_loss_t'):
             fx = _make_with_custom_variables(prob,x)
             f_array = f_array.write(t,fx)
         with tf.name_scope('Unroll_delta_state_update'):
             delta,s_c_out,s_h_out = update_state(fx, x, state_c,state_h)
         with tf.name_scope('Unroll_Optimizee_update'):
-            x_new = [x_n + d for x_n,d in zip(x_new,delta)]
+            x_new = [x_n + d for x_n,d in zip(x,delta)]
         t_new = t + 1
 
         return t_new, f_array, x_new,s_c_out,s_h_out
 
-    fx_array = tf.TensorArray(tf.float32, size=unroll_nn + 1,
+    fx_array = tf.TensorArray(tf.float32, size=unroll_nn,
                               clear_after_read=False)
     _, fx_array, x_final, S_C,S_H = tf.while_loop(
-        cond=lambda t, *_: t < unroll_nn,
+        cond=lambda t, *_: t < unroll_nn-1,
         body=time_step,
         loop_vars=(0, fx_array, opt_var, state_c,state_h),
         parallel_iterations=1,
@@ -203,11 +197,21 @@ def metaopti(prob):
         name="unroll")
     with tf.name_scope('Unroll_loss_period'):
         fx_final = _make_with_custom_variables(prob, x_final)
-        fx_array = fx_array.write(unroll_nn, fx_final)
+        fx_array = fx_array.write(unroll_nn-1, fx_final)
         arrayf = fx_array.stack()
 
     with tf.name_scope('Metaloss'):
         loss_optimizer = tf.reduce_sum(fx_array.stack())
+
+    with tf.name_scope('MetaOpt'):
+        optimizer = tf.train.AdamOptimizer(lr)
+
+    with tf.name_scope('Meta_update'):
+        step = optimizer.minimize(loss_optimizer)
+        # gvs = optimizer.compute_gradients(loss_optimizer)
+        # grads, tvars = zip(*gvs)
+        # clipped_grads,_ = tf.clip_by_global_norm(grads, 5.0)
+        # step = optimizer.apply_gradients(zip(grads, tvars))
 
     with tf.name_scope('state_optimizee_var'):
         variables = (nest.flatten(state_c) + nest.flatten(state_h) + opt_var)
@@ -219,25 +223,16 @@ def metaopti(prob):
         update = (nest.flatten([tf.assign(r,v) for r,v in zip(opt_var,x_final)]) +
                   (nest.flatten([tf.assign(r,v) for r,v in zip(state_c[i],S_C[i]) for i in range(len(state_c))])) +
                   (nest.flatten([tf.assign(r, v) for r, v in zip(state_h[i], S_H[i]) for i in range(len(state_h))])))
-
-    with tf.name_scope('MetaOpt'):
-        optimizer = tf.train.AdamOptimizer(lr)
-
-    with tf.name_scope('Meta_update'):
-        gvs = optimizer.compute_gradients(loss_optimizer)
-        grads, tvars = zip(*gvs)
-        clipped_grads,_ = tf.clip_by_global_norm(grads, 5.0)
-        step = optimizer.apply_gradients(zip(clipped_grads, tvars))
     return step, loss_optimizer, update, reset, fx_final, arrayf, x_final
 
-def run_epoch(sess, num_iter, arraycost, ops, reset):
+def run_epoch(sess, num_iter, arraycost, cost_op, ops, reset):
   sess.run(reset)
   costepoch = []
   """Runs one optimization epoch."""
   for _ in range(num_iter):
-     cost= sess.run([arraycost] + ops)[0]
+     cost, loss= [sess.run([arraycost,cost_op] + ops)[j] for j in range(2)]
      costepoch.append(np.log10(cost))
-  return costepoch
+  return np.reshape(costepoch,-1), loss
 
 def print_stats(header, total_error_optimizee, total_time):
   """Prints experiment statistics."""
@@ -258,38 +253,39 @@ with tf.Session() as sess:
     num_iter = num_steps // unroll_nn
     losstrain = []
     losseval = []
+    plotlosstrain = []
+    plotlosseval = []
     #Training
     for e in range(n_epoch):
-        cost= run_epoch(sess, num_iter, arraycost, [update, step], reset)
+        cost, trainloss= run_epoch(sess, num_iter, arraycost, cost_op, [step,update], reset)
         print(cost)
         losstrain.append(cost)
-        print_stats("Training Epoch {}".format(e), cost, timer() - start)
+        print_stats("Training Epoch {}".format(e), trainloss, timer() - start)
         saver = tf.train.Saver()
 
         if (e + 1) % evaluation_period == 0:
             for _ in range(evaluation_epochs):
-                evalcost = run_epoch(sess, num_iter, arraycost, [update], reset)
+                evalcost,evaloss = run_epoch(sess, num_iter, arraycost, cost_op, [update], reset)
                 losseval.append(evalcost)
-            if save_path is not None and evalcost[unroll_nn+1] < best_evaluation:
+            if save_path is not None and evaloss < best_evaluation:
                 print("Saving meta-optimizer to {}".format(save_path))
-                saver.save(sess, save_path,global_step=e)
-                best_evaluation = evalcost[unroll_nn+1]
-    # slengths = np.arange(n_epoch)
-    # plt.figure(figsize=(8, 5))
-    # plt.plot(slengths, losstrain, 'r-', label='Training Loss')
-    # plt.xlabel('Epoch')
-    # plt.ylabel('Training Loss')
-    # plt.legend()
-    # savefig('Training.png')
-    # plt.close()
-    # slengths = np.arange(len(losseval))
-    # plt.figure(figsize=(8, 5))
-    # plt.plot(slengths, losseval, 'b-', label='Validation Loss')
-    # plt.xlabel('Epoch')
-    # plt.ylabel('Validation Loss')
-    # plt.legend()
-    # savefig('Validation.png')
-    # plt.close()
+                saver.save(sess, save_path,global_step=0)
+                best_evaluation = evaloss
+                plotlosstrain.append(cost)
+                plotlosseval.append(evalcost)
+    slengths = np.arange(num_steps)
+    plt.figure(figsize=(8, 5))
+    plt.plot(slengths, plotlosstrain[-1], 'r-', label='Training Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Training Loss')
+    plt.legend()
+    savefig('Training.png')
+    plt.close()
+    plt.figure(figsize=(8, 5))
+    plt.plot(slengths, plotlosseval[-1], 'b-', label='Validation Loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Validation Loss')
+    plt.legend()
+    savefig('Validation.png')
+    plt.close()
     graph_writer.close()
-    #os.system('tensorboard --logdir=/Users/kalyanb/PycharmProjects/MetaLearning/MetaLog')
-    # os.system('-m webbrowser -t "http://bairstow:6006/#graphs"')

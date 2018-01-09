f2cd9ee6986a8b8c180a66b2eba7afe5a436269a
Kalyan Bhattacharjee <kalyan.iitkgp08@gmail.com>
2018-01-09 11:18:35 +1100

    Updated

--- Latest JMD/code/Postprocess_HV.m
+++ Latest JMD/code/Postprocess_HV.m
@@ -3,15 +3,12 @@ load(strcat(pwd,filesep,'Results',filesep,'vehicle_architecture_ori_ws',filesep,
 archive1 = archive;
 load(strcat(pwd,filesep,'Results',filesep,'vehicle_architecture_ori',filesep,'Obj-5',filesep,'run-1',filesep,'Archive.mat'));
 archive2 = archive;
-load(strcat(pwd,filesep,'Results',filesep,'vehicle_architecture_ori_n',filesep,'Obj-5',filesep,'run-1',filesep,'Archive.mat'));
-archive3 = archive;
 prob = load_problem_definition(def);
-costall = [0 200 500 1000 2000 3000 4000];
+costall = [0 200 500 1000 2000];
 numnd = [];
 archivei1 = archive1(1:costall(end),:);
 archivei2 = archive2(1:costall(end),:);
-archivei3 = archive3(1:costall(end),:);
-archiveall = [archivei1;archivei2;archivei3];
+archiveall = [archivei1;archivei2];
 fall = archiveall(archiveall(:,end) == 0,3+prob.nx:2+prob.nx+prob.nf);
 xall = archiveall(archiveall(:,end) == 0,2:1+prob.nx);
 [xa,idxu] = unique(xall,'rows');
@@ -21,7 +18,7 @@ xa_all = xa(front(1).f,:);
 fa_all = fa(front(1).f,:);
 ideal = min(fa_all,[],1);
 nadir = max(fa_all,[],1);
-numhv1 = []; numhv2 = [];numhv3 = [];
+numhv1 = []; numhv2 = [];
 pathtest = strcat(pwd,filesep,'Methods',filesep,'Hypervolume',filesep,'test.exe');
 for i = 1:numel(costall)
 % 1st archive
@@ -80,37 +77,12 @@ for i = 1:numel(costall)
     else
         numhv2 = [numhv2;0];
     end
-   % 3rd archive
-    archivei33 = archivei3(1:costall(i),:);
-    xall = [];fall = [];
-    xall = archivei33(archivei33(:,end) == 0,2:1+prob.nx);
-    fall = archivei33(archivei33(:,end) == 0,3+prob.nx:2+prob.nx+prob.nf);
-    [xa3,idxu] = unique(xall,'rows');
-    fa3 = fall(idxu,:);
-    [front,~] = nd_sort(fa3,(1:size(fa3,1))');
-    if ~isempty(front)
-        f3 = fa3(front(1).f,:);
-        f3a = set_extract(f3,1.1*nadir,0);
-        f3s = value2quantile(f3a,1,[ideal;nadir]);
-        commandhvarchive = strcat('"',pathtest,'"',' "fndarchive3',num2str(i),'.txt"',{' '},refstr);
-        fp = fopen(strcat('fndarchive3',num2str(i),'.txt'),'w');
-        fprintf(fp,'%s\r\n','#');
-        fprintf(fp,strcat(str,'\r\n'),f3s');
-        fprintf(fp,'%s','#');
-        fclose(fp);
-        [~,hv3char] = system(commandhvarchive{1});
-        hv3 = str2double(hv3char);
-        numhv3 = [numhv3;hv3];
-    else
-        numhv3 = [numhv3;0];
-    end
 end
 plot(costall,numhv2','ro-');hold on;
 plot(costall,numhv1','bo-');
-plot(costall,numhv3','mo-');
 xlabel('Evaluation Cost','fontname','arial','fontsize',16);
 ylabel('Hypervolume','fontname','arial','fontsize',16);
-legendtxt = {'SaMaO-ASF+ED','MaO','NSGA-II'};
+legendtxt = {'SaMaO-ASF+ED','MaO'};
 legend(legendtxt,'fontname','arial','fontsize',16);
 set(gca,'fontname','arial','fontsize',16);
 grid on
